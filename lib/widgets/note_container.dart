eda95f4eca4c860ca45c29b82a395b73b0bd8585
Osama <osama.ali.lasheen@gmail.com>
2024-12-03 18:38:40 +0200

    Refactor NoteContainer layout and styling
    Updated the CustomNoteContainer widget to use a Column and ListTile for better layout and styling. Removed unnecessary padding and adjusted text styling. Commented out the old layout for reference.

--- lib/widgets/note_container.dart
+++ lib/widgets/note_container.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -13,54 +15,83 @@ class CustomNoteContainer extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        height: 230,
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         width: 400,
         decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.circular(20),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Row(
-            children: [
-              const Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'note.title',
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      'note.description',
-                      style: TextStyle(fontSize: 20),
-                      maxLines: 4,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+          ListTile(
+              title: const Text(
+                'note.titlenote',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              subtitle: const Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'note.description',
+                  maxLines: 4,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-              const SizedBox(
-                width: 20,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    iconSize: 50,
-                    onPressed: onDelete,
-                    icon: const Icon(Icons.delete),
-                  ),
-                  Text(date.toString()),
-                ],
-              ),
-            ],
+              trailing: IconButton(
+                iconSize: 50,
+                onPressed: onDelete,
+                icon: const Icon(Icons.delete),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(date.toString()),
           ),
-        ),
+        ]),
       ),
     );
   }
 }
+//  Padding(
+//           padding: const EdgeInsets.all(24.0),
+//           child: Row(
+//             children: [
+//               const Expanded(
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       'note.title',
+//                       maxLines: 1,
+//                       overflow: TextOverflow.ellipsis,
+//                       style:
+//                           TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+//                     ),
+//                     SizedBox(height: 12),
+//                     Text(
+//                       'note.description',
+//                       style: TextStyle(fontSize: 20),
+//                       maxLines: 4,
+//                       overflow: TextOverflow.ellipsis,
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//               const SizedBox(
+//                 width: 20,
+//               ),
+//               Column(
+//                 crossAxisAlignment: CrossAxisAlignment.end,
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   IconButton(
+//                     iconSize: 50,
+//                     onPressed: onDelete,
+//                     icon: const Icon(Icons.delete),
+//                   ),
+//                   Text(date.toString()),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         )
